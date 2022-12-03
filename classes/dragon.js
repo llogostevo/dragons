cc691901535a2967200247a39081ac254681caf6
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-12-03 16:34:00 +0000

    completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,31 @@
 // Your code here
+class Dragon{
+  constructor(name, color){
+    this.name = name;
+    this.color =color;
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons){
+    return dragons.map(dragon => dragon.name)
+  }
+
+  // my experimentation with static methods
+  static dragonsBreathFire(...dragons){
+    dragons.forEach(dragon => console.log(dragon.breathesFire()))
+  }
+}
+
+
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
+
+console.log(Dragon.dragonsBreathFire(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,42 @@
 // Your code here
+const Dragon = require('../classes/dragon.js');
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach((lifeGoal) =>{
+            console.log(`${this.name} likes to ${lifeGoal}`)
+        })
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+
+// exports
+try {
+    module.exports = FriendlyDragon;
+  } catch {
+    module.exports = null;
+  }
